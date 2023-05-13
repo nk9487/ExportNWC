f88b732ced03d5dd10e0f6880bd33911dd720d49
Namuna Kassaye <65698290+nk9487@users.noreply.github.com>
2023-05-13 09:55:34 -0700

    cross check (#8)
    * user interface for export added
    
    * Export function updated selected views and WPF user interface added
    
    * UI uodated to include project list, read project export setting from json file
    
    * updated UI and added project setting read capability
    
    * Repo Cleaned

--- NwcOut.cs
+++ NwcOut.cs
@@ -52,53 +52,7 @@ namespace ExportNWC
             //return Autodesk.Revit.UI.Result.Succeeded;
         }
 
-        /*public void ExportNWC()
-        {
-            uiapp = CommandData.Application;
-            uidoc = uiapp.ActiveUIDocument;
-            app = uiapp.Application;
-            doc = uidoc.Document;
-            NavisworksExportOptions nwcOptions = new NavisworksExportOptions();
-            nwcOptions.ConvertElementProperties = true;
-            nwcOptions.ConvertLinkedCADFormats = true;
-            nwcOptions.Coordinates = NavisworksCoordinates.Internal;//Desfualt is shared
-            nwcOptions.DivideFileIntoLevels = true;
-            nwcOptions.ExportElementIds = true;
-            nwcOptions.ExportLinks = true;
-            nwcOptions.ExportParts = true;
-            nwcOptions.ExportRoomAsAttribute = true;
-            nwcOptions.ExportRoomGeometry = true;
-            nwcOptions.ExportScope = NavisworksExportScope.Model; // Check 
-            nwcOptions.ExportUrls = true;
-            nwcOptions.FacetingFactor = 1.0;
-            nwcOptions.FindMissingMaterials = true;
-            nwcOptions.ViewId = doc.ActiveView.Id;
-            
-            nwcOptions.Parameters = NavisworksParameters.All;
-
-            try
-            {
-                //Selecting current view
-                Selection select = uidoc.Selection;
-                if (doc.ActiveView.Document == null)
-                {
-                    // If no elements selected.
-                    TaskDialog.Show("Revit", "Currenyt view is null");
-                }
-                else
-                {
-                    doc.Export(@"C:\Users\namun\OneDrive\Desktop\NWC", @"test.nwc", nwcOptions); ;
-                    TaskDialog.Show("Revit", "Export Completed");
-
-                }
-
-            }
-
-            catch (System.Exception e)
-            {
-                message = e.Message;    
-            }
-        }*/
+       
 
     }
 }
